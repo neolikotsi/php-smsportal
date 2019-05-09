4302b1e0d60a737857e469349dc2dc05eec4ab31
bitbuket.org/sleepsolutions <neo@sleepsolutions-sa.com>
2019-05-09 10:30:13 +0200

    update namespace

---
+++ src/RestClient.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace NeoLikotsi\SMSPortal;
+
+class RestClient
+{
+    /**
+     * @var string
+     */
+    const HTTP_GET = 'GET';
+    /**
+     * @var string
+     */
+    const HTTP_POST = 'POST';
+    /**
+     * @var string
+     */
+    private $baseRestUri;
+    private $apiToken;
+    private $client;
+    private $apiId;
+    private $apiSecret;
+
+    /**
+     * Create a new API connection
+     *
+     * @param string $apiToken The token found on your integration
+     */
+    public function __construct(string $apiId, string $apiSecret, string $baseRestUri = 'https://rest.smsportal.com/v1/')
+    {
+        $this->client = new \GuzzleHttp\Client;
+        $this->apiId = $apiId;
+        $this->apiSecret = $apiSecret;
+        $this->baseRestUri = $baseRestUri;
+    }
+
+    public function message() : RestClient
+    {
+        return $this;
+    }
+
+    /**
+     * get apiToken
+     *
+     * @return $this
+     */
+    public function authorize()
+    {
+        $response = $this->client->request(static::HTTP_GET, $this->baseRestUri . 'Authentication', [
+            'http_errors' => false,
+            'headers' => ['Authorization' => 'Basic ' . base64_encode($this->apiId . ':' . $this->apiSecret)]
+        ]);
+        $responseData = $this->getResponse($response->getBody());
+        $this->apiToken = $responseData['token'];
+        return $this;
+    }
+
+    /**
+     * Submit API request to send SMS
+     *
+     * @param array $options
+     * @return array
+     */
+    public function send(array $options)
+    {
+        $this->authorize();
+        $requestBody = ['messages' => [$options]];
+        $response = $this->client->request(static::HTTP_POST, $this->baseRestUri . 'BulkMessages', [
+            'json' => $requestBody,
+            'http_errors' => false,
+            'headers' => ['Authorization' => 'Bearer ' . $this->apiToken]
+        ]);
+        return $this->getResponse($response->getBody());
+    }
+
+    public function balance()
+    {
+        $response = $this->client->request(static::HTTP_GET, $this->baseRestUri . 'Balance', [
+            'http_errors' => false,
+            'headers' => ['Authorization' => 'Bearer ' . $this->apiToken]
+        ]);
+        $responseData = $this->getResponse($response->getBody());
+        return $responseData['number'];
+    }
+
+    /**
+     * Tranform response string to responseData
+     *
+     * @param string $responseBody
+     * @return array
+     */
+    private function getResponse(string $responseBody)
+    {
+        return json_decode($responseBody, true);
+    }
+}
